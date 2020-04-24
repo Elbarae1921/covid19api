731d791a77f034def2523ec49647a3555b39e2d1
Elbarae <elbarae1921@gmail.com>
2020-04-24 08:05:11 +0000

    first commit

---
+++ dataScraper.js
@@ -0,0 +1,94 @@
+const rp = require("request-promise");
+const $ = require("cheerio");
+
+
+
+const url = "https://www.worldometers.info/coronavirus/";
+const selector = "#main_table_countries_today tbody";
+
+const getCountriesArray = () => {
+    return new Promise(resolve => {
+        console.log("scraping the page...");
+        rp(url)
+            .then(html => {
+                const data = $(selector, html);
+                //console.log(data.get(0).children.filter(x => x.hasOwnProperty('children')).length);
+                const countriesData = data.get(0).children.filter(x => x.hasOwnProperty('children')).map(x => {
+                    if (!x.children[1].children[0].hasOwnProperty("children")) {
+                        return x.children[1].children[0].data.trim();
+                    }
+                    else {
+                        return x.children[1].children[0].children[0].data.trim();
+                    }
+                });
+                console.log("scraping done!");
+                resolve(countriesData);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+    
+}
+
+const getDataArray = async () => {
+    return new Promise(resolve => {
+        const dataArray = [];
+        rp(url)
+            .then(html => {
+                const data = $("#maincounter-wrap span", html);
+                dataArray.push(data.get(0).children[0].data.trim());
+                dataArray.push(data.get(1).children[0].data.trim());
+                dataArray.push(data.get(2).children[0].data.trim());
+                resolve(dataArray);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    })
+    
+}
+
+const getCountryDataArray = async country => {
+    return new Promise(resolve => {
+        rp(url)
+            .then(html => {
+                const dataArray = [];
+                const data = $(selector, html);
+                console.log(data.get(0).children.filter(x => x.hasOwnProperty('children')).length);
+                const vals = data.get(0).children.filter(x => x.hasOwnProperty('children')).filter(x => {
+                    if (!x.children[1].children[0].hasOwnProperty("children")) {
+                        if(x.children[1].children[0].data.trim() == country)
+                            return true;
+                        else
+                            return false
+                    }
+                    else {
+                        if(x.children[1].children[0].children[0].data.trim() == country)
+                            return true;
+                        else
+                            return false;
+                    }
+                });
+                //console.log(vals[0].children[11].children[0].data);
+                //console.log(`Total Cases : ${vals[0].children[3].children[0].data}\nNew Cases : ${vals[0].children[7].children[0].data}\nTotal Deaths : ${vals[0].children[9].children[0].data}\nNew Deaths : ${vals[0].children[11].children[0].data}\nTotal Recovered ${vals[0].children[13].children[0].data}\nActive Cases : ${vals[0].children[17].children[0].data}\nSerious/Critical Cases : ${vals[0].children[19].children[0].data}`);
+                dataArray.push(vals[0].children[3].hasOwnProperty("children") ? vals[0].children[3].children.length > 0 ?  vals[0].children[3].children[0].data : "" : "");
+                dataArray.push(vals[0].children[5].hasOwnProperty("children") ? vals[0].children[5].children.length > 0 ?  vals[0].children[5].children[0].data : "" : "");
+                dataArray.push(vals[0].children[7].hasOwnProperty("children") ? vals[0].children[7].children.length > 0 ?  vals[0].children[7].children[0].data : "" : "");
+                dataArray.push(vals[0].children[9].hasOwnProperty("children") ? vals[0].children[9].children.length > 0 ?  vals[0].children[9].children[0].data : "" : "");
+                dataArray.push(vals[0].children[11].hasOwnProperty("children") ? vals[0].children[11].children.length > 0 ?  vals[0].children[11].children[0].data : "" : "");
+                dataArray.push(vals[0].children[13].hasOwnProperty("children") ? vals[0].children[13].children.length > 0 ?  vals[0].children[13].children[0].data : "" : "");
+                dataArray.push(vals[0].children[15].hasOwnProperty("children") ? vals[0].children[15].children.length > 0 ?  vals[0].children[17].children[0].data : "" : "");
+                resolve(dataArray);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+}
+
+module.exports = {
+    getCountriesArray,
+    getDataArray,
+    getCountryDataArray
+}
---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const Scraper = require('./dataScraper');
+
+const PORT = 5000;
+
+const server = express();
+
+server.get('/', (req, res) => {
+    Scraper.getDataArray().then(data => {
+        res.json({
+            "Total Cases": data[0],
+            "Total Recoveries": data[2],
+            "Total Deaths": data[1]
+        });
+    })
+});
+
+server.get('/:country', (req, res) => {
+    let country = req.params.country;
+    if (country) {
+        country = country.replace(/\s/g, "");
+        Scraper.getCountriesArray()
+            .then(countries => {
+                if (countries.find(c => c == country)) {
+                    Scraper.getCountryDataArray(country)
+                        .then(data => {
+                            res.json({
+                                "Country": country,
+                                "Total Cases": data[0],
+                                "New Cases": data[1],
+                                "Total Deaths": data[2],
+                                "New Deaths": data[3],
+                                "Total Recovered": data[4],
+                                "Active Cases": data[5]
+                            });
+                        });
+                }
+                else {
+                    res.json({
+                        error: "No info on the provided country"
+                    });
+                }
+            });
+    }
+    else {
+        res.json({
+            error: "please provide a country"
+        });
+    }
+});
+
+server.get('/*', (req, res) => {
+    res.json({
+        routes: [
+            {
+                route: "/",
+                desc: "Coronavirus stats worldwide"
+            },
+            {
+                route: "/:country",
+                desc: "Coronavirus stats in the specified country"
+            }
+        ]
+    });
+});
+
+server.listen(PORT, () => console.log(`listening on ${PORT}...`));
